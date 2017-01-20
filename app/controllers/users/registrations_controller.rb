a9869ba59e253f03a28e4d6a3d27a4098d2aa99c
toyoshi <toyosystem@gmail.com>
2017-01-20 11:57:26 +0900

    Fix bug

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,2 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_registable, only: [:new, :create]
-
-  protected
-
-  def check_registable
-    #Is first user
-    return true if User.count == 0
-
-    #TOKEN is valid
-    token = params[:rt] || session[:rt]
-    if token == Option.user_registration_token
-      session[:rt] = token
-    else
-      redirect_to root_path, notice: 'invalid token' 
-    end
-  end
 end
