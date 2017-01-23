1b4f1ee27f55692f71014dc799a6525cd2aea0b4
toyoshi <toyosystem@gmail.com>
2017-01-23 15:03:09 +0900

    エディットページの遷移防止スクリプトを削除
    期待しないページでも動作してしまっていたので一旦削除する。

--- app/assets/javascripts/simplemde.js
+++ app/assets/javascripts/simplemde.js
@@ -11,29 +11,4 @@ document.addEventListener("turbolinks:load", function() {
     element: $('#word_body')[0],
     spellChecker: false
   });
-
-  var unsaved = false;
-  var formWarningMessage = 'Are you sure?';
-  simplemde.codemirror.on("change", function(){
-    unsaved = true; 
-  });
-
-  $('a').click(function(e){
-    var href = $(this).attr('href')
-    if(typeof(href) !== "undefined" && href != '#' && unsaved){
-      if(confirm(formWarningMessage) == false){
-        e.preventDefault()
-      }
-    }
-  });
-
-  $(window).on('beforeunload', function(){
-    if(unsaved){
-      return formWarningMessage
-    }
-  });
-
-  $('input[type=submit]').on('click', function(e) { 
-    $(window).off('beforeunload'); 
-  }); 
 });
